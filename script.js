7f71f4ea87343935b363b92b3ccc979f38d392dc
biancabagnara <108936801+biancabagnara@users.noreply.github.com>
2023-10-30 08:14:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ raiz = x => Math.sqrt (x);
 //mensagem( soma(3, div(2,2)));
 //mensagem( soma(div80,2),mult(5,3));
 //mensagem( soma(sub(8,5),sub(3,70)));
+
+let a = " ";
+let b = " ";
+let c = " ";
+let sa = "+";
+let sb = "+";
+let sc = "+";
+let valor = " ";
+let temPonto = false;
+let desligada = true;
+
+function porcentagem(){
+    if (executa == "mult"){
+        b = valor;
+        escrever_display(div(mult(a,b),100));
+        b = "";
+        valor ='';
+    }
+
+}
